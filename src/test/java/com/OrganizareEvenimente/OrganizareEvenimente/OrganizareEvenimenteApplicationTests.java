c6d536757b1c6aba34313601865ea16d4b9b01e4
CarinaBoiangiu <carina.boiangiu10@gmail.com>
2025-11-30 23:17:42 +0200

    init

---
+++ src/test/java/com/OrganizareEvenimente/OrganizareEvenimente/OrganizareEvenimenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrganizareEvenimente.OrganizareEvenimente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizareEvenimenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
